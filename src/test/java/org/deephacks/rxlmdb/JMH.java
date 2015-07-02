46bb8cca57d4b6a946ba16d96310d4ac060d7097
Kristoffer Sjogren <stoffe@gmail.com>
2015-07-02 16:59:27 +0200

    simplistic jmh test comparing scan for plain and rx

--- src/test/java/org/deephacks/rxlmdb/JMH.java
+++ src/test/java/org/deephacks/rxlmdb/JMH.java
@@ -1,16 +1,82 @@
 package org.deephacks.rxlmdb;
 
+import org.fusesource.lmdbjni.BufferCursor;
+import org.fusesource.lmdbjni.DirectBuffer;
+import org.fusesource.lmdbjni.Transaction;
 import org.openjdk.jmh.annotations.*;
+import rx.Observable;
+import rx.Subscriber;
 
+import java.nio.ByteOrder;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Thread)
+@Measurement(iterations = 5)
+@Warmup(iterations = 10)
+@Fork(value = 2)
 public class JMH {
+  RxDB db;
+  RxLMDB lmdb;
 
-  @Benchmark
-  public void test() {
+  @Setup()
+  public void setup() {
+    lmdb = RxLMDB.builder().size(ByteUnit.GIGA, 1).build();
+    db = RxDB.builder().lmdb(lmdb).build();
+    Observable<KeyValue> observable = Observable.create(new Observable.OnSubscribe<KeyValue>() {
+      @Override
+      public void call(Subscriber<? super KeyValue> subscriber) {
+        for (int j = 0; j < 4; j++) {
+          DirectBuffer key = new DirectBuffer(new byte[5]);
+          for (int i = 0; i < 500_000; i++) {
+            key.putByte(0, (byte) j);
+            key.putInt(1, i, ByteOrder.BIG_ENDIAN);
+            subscriber.onNext(new KeyValue(key.byteArray(), key.byteArray()));
+          }
+          System.out.println(j);
+        }
+        subscriber.onCompleted();
+        System.out.println("commit done");
+      }
+    }).doOnError(throwable -> throwable.printStackTrace());
+    db.put(observable);
   }
 
+  Iterator<List<KeyValue>> obs;
+  Iterator<KeyValue> values;
+
+  @Benchmark
+  public void rx() {
+    if (obs == null) {
+      obs = db.scan().toBlocking().toIterable().iterator();
+      values = obs.next().iterator();
+    }
+    if (values.hasNext()) {
+      values.next();
+    } else if (obs.hasNext()) {
+      values = obs.next().iterator();
+    } else {
+      obs = db.scan().toBlocking().toIterable().iterator();
+      values = obs.next().iterator();
+    }
+  }
+
+  BufferCursor cursor;
+  Transaction tx;
+  
+  @Benchmark
+  public void plain() {
+    if (cursor == null) {
+      tx = lmdb.env.createReadTransaction();
+      cursor = db.db.bufferCursor(tx);
+    }
+    if (cursor.next()) {
+      new KeyValue(cursor.keyBytes(), cursor.valBytes());
+    } else {
+      cursor.first();
+    }
+  }
 }

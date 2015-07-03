18f5b00a0460f19ee13f9bac22321342bef7a8bd
Kristoffer Sjogren <stoffe@gmail.com>
2015-07-03 12:00:51 +0200

    correct jmh script

--- jmh/jmh.sh
+++ jmh/jmh.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mvn clean install -P jmh
-java -cp target/rxlmdb-0.0.2-SNAPSHOT-tests.jar:target/jars/jmh-core-1.10.jar:target/benchmarks.jar:target/jars/jopt-simple-4.6.jar:target/jars/commons-math3-3.2.jar:target/jars/lmdbjni-linux64-0.4.1.jar org.openjdk.jmh.Main
+mvn clean install && java -jar target/benchmarks.jar

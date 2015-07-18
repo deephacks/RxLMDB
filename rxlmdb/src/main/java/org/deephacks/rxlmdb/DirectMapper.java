88988d6662429d7d42b3cd38147d2bb7845e281a
Kristoffer Sjogren <stoffe@gmail.com>
2015-07-18 20:05:57 +0200

    javadoc for direct mapper

--- rxlmdb/src/main/java/org/deephacks/rxlmdb/DirectMapper.java
+++ rxlmdb/src/main/java/org/deephacks/rxlmdb/DirectMapper.java
@@ -2,6 +2,14 @@ package org.deephacks.rxlmdb;
 
 import org.fusesource.lmdbjni.DirectBuffer;
 
+/**
+ * Allow for zero-copy using addresses provided by LMDB instead of
+ * copying data for each operation. Buffer addresses are valid for the
+ * duration of a transaction. Accessing buffers outside a transaction
+ * will cause SIGSEGV.
+ *
+ * @param <T> The mapped type.
+ */
 public interface DirectMapper<T> {
   /**
    * Provide a zero copy key/value pair applied to a given result.
